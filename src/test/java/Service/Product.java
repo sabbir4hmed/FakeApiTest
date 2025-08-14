21742b7ece8dad6d1f3b1d790a44d367e582ca54
DEADMAUS <sabbir4hmed@yahoo.com>
2025-08-14 17:56:11 +0600

    add product test

---
+++ src/test/java/Service/Product.java
@@ -0,0 +1,62 @@
+package Service;
+
+import Base.BaseService;
+import io.restassured.response.Response;
+
+import java.util.*;
+
+public class Product extends BaseService {
+
+    public Response getallproducts()
+    {
+        return request.get("/products");
+    }
+
+    public List<Response> getproductbysingleid()
+    {
+        int i, totalid = 20;
+
+        List<Response> responses = new ArrayList<>();
+
+        for(i = 1; i<= totalid; i++)
+        {
+          Response response =  request.get("/products/" + i);
+          responses.add(response);
+
+        }
+
+        return responses;
+    }
+
+
+    // Add a new Product
+
+
+    public Response createproduct()
+    {
+
+        Map<String, Object> data = new HashMap<>();
+
+        data.put("id", 21);
+        data.put("title", "Denim Pant");
+        data.put("price", 60.25);
+        data.put("description", "This is the denim pant");
+        data.put("category", "men's clothing");
+        data.put("image", "https://images.othoba.com/images/thumbs/0547350_mens-slim-fit-stretchable-denim-jeans-pant-deep-blue.webp");
+
+        Map<String, Object> prodcutrating = new HashMap<>();
+
+        prodcutrating.put("rate", 4.3);
+        prodcutrating.put("count", 300);
+
+        data.put("rating", prodcutrating);
+
+
+        return request
+                .body(data)
+                .post("/products/");
+    }
+
+
+
+}
--- src/test/java/Tests/ProductTest.java
+++ src/test/java/Tests/ProductTest.java
@@ -1,6 +1,6 @@
 package Tests;
 
-import Service.GetProduct;
+import Service.Product;
 import io.restassured.response.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,16 +9,16 @@ import org.testng.annotations.Test;
 
 import java.util.List;
 
-public class GetProductTest
+public class ProductTest
 {
-    private static final Logger log = LoggerFactory.getLogger(GetProductTest.class);
-    GetProduct getProduct = new GetProduct();
+    private static final Logger log = LoggerFactory.getLogger(ProductTest.class);
+    Product product = new Product();
 
-        
+
         @Test(testName = "Get all products")
         public void testgetallproduct()
         {
-            Response response = getProduct.getallproducts();
+            Response response = product.getallproducts();
             Assert.assertEquals(response.getStatusCode(), 200);
             System.out.println("Getting Status Code is: " + response.getStatusCode());
             //response.prettyPrint();
@@ -31,7 +31,7 @@ public class GetProductTest
         @Test(testName = "Get Single product")
         public void testgetsingleproduct()
         {
-            List<Response> responses = getProduct.getproductbysingleid();
+            List<Response> responses = product.getproductbysingleid();
 
             for(int i = 0; i< responses.size(); i++)
             {
@@ -42,6 +42,16 @@ public class GetProductTest
                 System.out.println("Getting Status Code is: " + response.getStatusCode());
                 //response.prettyPrint();
             }
+        }
+
+        @Test(testName = "Add New Product")
+        public void testaddproduct()
+        {
+            Response response = product.createproduct();
+            Assert.assertEquals(response.getStatusCode(), 201);
+            log.info("Add Product");
+            System.out.println("Getting Status Code is: " + response.getStatusCode());
+            response.prettyPrint();
         }
 
 
