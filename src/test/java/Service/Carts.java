b4b01f1792478ce20eaa9fa3eba39fbe37b0171e
SABBIR <sadik3030@gmail.com>
2025-08-16 22:33:06 +0600

    Cart items updated

--- src/test/java/Service/Carts.java
+++ src/test/java/Service/Carts.java
@@ -3,6 +3,11 @@ package Service;
 import Base.BaseService;
 import io.restassured.response.Response;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Carts extends BaseService {
 
     public Response getallcart()
@@ -10,6 +15,64 @@ public class Carts extends BaseService {
         return request
                 .get("/carts");
     }
+
+    public List<Response> getsinglecart()
+    {
+        int i , totalid = 7;
+
+        List<Response> responses = new ArrayList<>();
+
+        for (i = 1; i<= totalid; i++)
+        {
+            Response response = request.get("/carts/"+i);
+            responses.add(response);
+
+        }
+        return responses;
+
+    }
+
+    public Response createcart()
+    {
+        Map<String,Object> data = new HashMap<>();
+
+        data.put("id", 8);
+        data.put("userId", 8);
+        data.put("date", "2025-08-16T00:00:00.000Z");
+
+        Map<String, Object> product = new HashMap<>();
+
+        product.put("productId", 19);
+        product.put("quantity", 10);
+
+        data.put("products", product);
+
+        return request
+                .body(data)
+                .post("/carts/");
+
+    }
+
+    public Response updatecart()
+    {
+        Map<String,Object> data = new HashMap<>();
+
+        data.put("id", 2);
+        data.put("userId", 1);
+        data.put("date", "2021-01-02T00:00:00.000");
+
+        Map<String,Object> product = new HashMap<>();
+
+        product.put("productId", 3);
+        product.put("quantity", 5);
+
+        data.put("products", product);
+
+        return request
+                .body(data)
+                .put("/carts/"+ 2);
+    }
+
 
 
 
--- src/test/java/Tests/CartTest.java
+++ src/test/java/Tests/CartTest.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class CartTest {
 
     Carts cartTest = new Carts();
@@ -22,6 +24,42 @@ public class CartTest {
         System.out.println("The status code is" + response.statusCode());
     }
 
+    @Test(testName = "Get single status code", priority = 2)
+    public void getsingleproductcart()
+    {
+        List<Response> responses = cartTest.getsinglecart();
+
+        for (int i = 1; i < responses.size(); i++)
+        {
+            Response response = responses.get(i);
+            Assert.assertEquals(response.getStatusCode(),200);
+            log.info("Get Single Cart");
+            System.out.println("The status code is: "+ response.statusCode());
+        }
+
+
+    }
+
+    @Test(testName = "Add product in Cart", priority = 1)
+    public void createsingleproduct()
+    {
+        Response response = cartTest.createcart();
+        Assert.assertEquals(response.getStatusCode(), 201);
+        log.info("Ädd product in cart");
+        System.out.println("The status code is: " + response.statusCode());
+        response.prettyPrint();
+    }
+
+
+    @Test(testName = "Updae the product cart", priority = 3)
+    public void updateproductcart()
+    {
+        Response response = cartTest.updatecart();
+        Assert.assertEquals(response.getStatusCode(), 200);
+        log.info("Upadte the product cart");
+        System.out.println("The status code is: " + response.statusCode());
+        response.prettyPrint();
+    }
 
 
 }
