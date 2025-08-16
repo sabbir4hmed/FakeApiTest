82fcac9ba576867a16619effc91c5024144d7175
SABBIR <sadik3030@gmail.com>
2025-08-16 20:47:53 +0600

    add cart items

--- src/test/java/Tests/CartTest.java
+++ src/test/java/Tests/CartTest.java
@@ -2,15 +2,24 @@ package Tests;
 
 import Service.Carts;
 import io.restassured.response.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class CartTest {
 
     Carts cartTest = new Carts();
 
+    private static final Logger log = LoggerFactory.getLogger(CartTest.class);
 
+    @Test(testName = "Get all Carts", priority = 1)
     public void getallcartlist()
     {
         Response response = cartTest.getallcart();
+        Assert.assertEquals(response.getStatusCode(),200);
+        log.info("Get all Carts");
+        System.out.println("The status code is" + response.statusCode());
     }
 
 
